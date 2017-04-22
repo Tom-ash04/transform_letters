4c5f9f9df1455a17edc43cc90777a177f66cb750
Tom-ash04 <tomy04@vp.pl>
2017-04-22 20:46:13 +0200

    chaining in function

--- pol.js
+++ pol.js
@@ -4,22 +4,6 @@ convert (text);
 
 function convert(){
 var str = text;
-var res = str.replace(/Ą/g, "A");
-str = str.replace(/Ć/g, "C");
-str = str.replace(/Ę/g, "E");
-str = str.replace(/Ł/g, "L");
-str = str.replace(/Ń/g, "N");
-str = str.replace(/Ó/g, "O");
-str = str.replace(/Ś/g, "S");
-str = str.replace(/Ź/g, "X");
-str = str.replace(/Ż/g, "Z");
-str = str.replace(/ą/g, "a");
-str = str.replace(/ć/g, "c");
-str = str.replace(/ę/g, "e");
-str = str.replace(/ł/g, "l");
-str = str.replace(/ń/g, "n");
-str = str.replace(/ó/g, "o");
-str = str.replace(/ź/g, "x");
-str = str.replace(/ż/g, "z");
-alert(str);
+var res = str.replace(/Ą/g, "A").replace(/Ć/g, "C").replace(/Ę/g, "E").replace(/Ł/g, "L").replace(/Ń/g, "N").replace(/Ó/g, "O").replace(/Ś/g, "S").replace(/Ź/g, "X").replace(/Ż/g, "Z").replace(/ą/g, "a").replace(/ć/g, "c").replace(/ę/g, "e").replace(/ł/g, "l").replace(/ń/g, "n").replace(/ó/g, "o").replace(/ź/g, "x").replace(/ż/g, "z");
+alert(res);
 }
